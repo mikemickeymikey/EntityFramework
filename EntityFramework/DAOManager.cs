e65557484877cfeb6424bde3a577b00ff2385872
Miguel Cuesta <a2106885@institutmontilivi.cat>
2023-08-16 18:57:02 +0200

    first commit

---
+++ EntityFramework/DAOManager.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data;
+using System.DirectoryServices.ActiveDirectory;
+using System.Linq;
+using System.Text;
+
+namespace EntityFramework
+{
+    class DAOManager
+    {
+        private MODEL.pelisContext context;
+        public DAOManager(MODEL.pelisContext context)
+        {
+            this.context = context;
+        }
+
+        public static DataTable DataTable(this DbContext context,
+           string sqlQuery, params DbParameter[] parameters)
+        {
+            DataTable dataTable = new DataTable();
+            DbConnection connection = context.Database.GetDbConnection();
+            DbProviderFactory dbFactory = DbProviderFactories.GetFactory(connection);
+            using (var cmd = dbFactory.CreateCommand())
+            {
+                cmd.Connection = connection;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = sqlQuery;
+                if (parameters != null)
+                {
+                    foreach (var item in parameters)
+                    {
+                        cmd.Parameters.Add(item);
+                    }
+                }
+                using (DbDataAdapter adapter = dbFactory.CreateDataAdapter())
+                {
+                    adapter.SelectCommand = cmd;
+                    adapter.Fill(dataTable);
+                }
+            }
+            return dataTable;
+        }
+
+        public object GetPelis()
+        {
+            List<MODEL.Film> lstPelis = context.Film.ToList();
+            return lstPelis;
+        }
+    }
+}
---
+++ EntityFramework/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EntityFramework
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MODEL.pelisContext m_db = new MODEL.pelisContext();
+        DAOManager manager;
+        public MainWindow()
+        {
+            InitializeComponent();
+            manager = new DAOManager(m_db);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            dgPelis.ItemsSource = manager.GetPelis();
+        }
+    }
+}
